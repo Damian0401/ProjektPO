214dd1af65dde34029d7dd0360020b9b368128bf
Damian <szkola_abc@o2.pl>
2021-04-07 20:09:13 +0200

    version 0.0.2

--- app/src/main/java/Epidemic_simulation/Human.java
+++ app/src/main/java/Epidemic_simulation/Human.java
@@ -1,11 +1,15 @@
 package Epidemic_simulation;
 
+import java.util.Random;
+
 public class Human extends SimulationObject{
     Human(int x, int y, int mapSize, int moveRange){
         super(x, y);
         this.mapSize = mapSize;
         this.moveRange = moveRange;
     }
+
+    private Random random = new Random();
 
     private int mapSize;
 
