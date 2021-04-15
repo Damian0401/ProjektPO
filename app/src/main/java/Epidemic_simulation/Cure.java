6a6adde2e7a933ff2b7b888ab5a42346bf7cdaae
Damian <szkola_abc@o2.pl>
2021-04-15 20:56:05 +0200

    version 0.0.8

--- app/src/main/java/Epidemic_simulation/Cure.java
+++ app/src/main/java/Epidemic_simulation/Cure.java
@@ -4,18 +4,8 @@ public class Cure extends SimulationObject{
     public Cure(int x, int y, int recoveryChance){
         super(x, y);
         this.recoveryChance = recoveryChance;
-        amount++;
     }
-    private static int amount;
-    private int recoveryChance;
-
-    public static int getAmount(){
-        return amount;
-    }
-
-    public static void decreaseAmount(){
-        amount--;
-    }
+    private final int recoveryChance;
 
     public int getRecoveryChance(){ return this.recoveryChance; }
 
