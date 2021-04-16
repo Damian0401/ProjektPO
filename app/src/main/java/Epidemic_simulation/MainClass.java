e18fc656dd783455895af17a87f6069e180e9fef
Damian <szkola_abc@o2.pl>
2021-04-16 16:40:06 +0200

    version 0.0.12

--- app/src/main/java/Epidemic_simulation/MainClass.java
+++ app/src/main/java/Epidemic_simulation/MainClass.java
@@ -3,7 +3,21 @@ package Epidemic_simulation;
 public class MainClass{
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        simulation(5,5,5,5,5,5,5,5,5);
 
+    }
+
+    public static void simulation(int stageNumber, int mapHeight, int mapWidth, int infectChance, int recoveryChance,
+                                  int healthyNumber, int infectedNumber, int medicalNumber, int cureNumber){
+        Epidemic epidemic = new Epidemic(mapHeight,mapWidth,infectChance,recoveryChance,healthyNumber,infectedNumber,medicalNumber,cureNumber);
+
+        epidemic.drawMap();
+        epidemic.saveStats();
+
+        for(int i = 0; i < stageNumber; i++){
+            epidemic.nextStage();
+            epidemic.drawMap();
+            epidemic.saveStats();
+        }
     }
 }
