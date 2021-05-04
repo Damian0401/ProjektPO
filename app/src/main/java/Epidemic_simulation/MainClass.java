fbfebd3f78ec340d7b627549709ab3a35263845d
Damian <szkola_abc@o2.pl>
2021-05-05 00:24:17 +0200

    Złe diagramy z epapu 4

--- app/src/main/java/Epidemic_simulation/MainClass.java
+++ app/src/main/java/Epidemic_simulation/MainClass.java
@@ -9,6 +9,7 @@ public class MainClass{
 
     public static void simulation(int stageNumber, int mapHeight, int mapWidth, int infectChance, int recoveryChance,
                                   int healthyNumber, int infectedNumber, int medicalNumber, int cureNumber){
+
         Epidemic epidemic = new Epidemic(mapHeight,mapWidth,infectChance,recoveryChance,healthyNumber,infectedNumber,medicalNumber,cureNumber);
 
         epidemic.drawMap();
